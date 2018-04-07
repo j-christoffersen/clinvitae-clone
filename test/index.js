96e6629b627553081b30573060c8f2642444b0cd
Jackson Christoffersen <jacksontchristo@gmail.com>
2018-04-07 16:55:09 -0700

    add tests for /api/search

--- test/index.js
+++ test/index.js
@@ -7,17 +7,6 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('/api/variants', () => {
-  describe('GET /', () => {
-    it('should respnd with 200 OK', (done) => {
-      chai.request(server)
-        .get('/')
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-
   describe('GET /api/variants', () => {
     it('should respond with 400 when no gene is specified', (done) => {
       chai.request(server)
@@ -35,6 +24,43 @@ describe('/api/variants', () => {
           res.should.have.status(200);
           res.body.should.be.a('array');
           res.body.length.should.be.eql(1);
+          done();
+        });
+    });
+  });
+});
+
+describe('/api/search', () => {
+  describe('GET /api/search', () => {
+    it('should respond with an empty array when there is no query', (done) => {
+      chai.request(server)
+        .get('./api/search')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
+
+    it('should respond with an empty array when the query is less than two characters', (done) => {
+      chai.request(server)
+        .get('./api/search?query=a')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
+
+    it('should respond with an array of genes when the query is two characters or longer', (done) => {
+      chai.request(server)
+        .get('./api/search?query=ab')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(4);
           done();
         });
     });
