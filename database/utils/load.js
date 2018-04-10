21313117be65fc6a560518bfd69b5190a272e4ec
Jackson Christoffersen <jacksontchristo@gmail.com>
2018-04-09 22:27:55 -0700

    refactor database loading script for decreased execution time

--- database/utils/load.js
+++ database/utils/load.js
@@ -4,68 +4,84 @@ const parse = require('csv-parse');
 
 const knex = require('../knex.js');
 
-const readStream = fs.createReadStream(process.env.DB_FILE_PATH)
-  .pipe(parse({
-    delimiter: '\t',
-    relax_column_count: true,
-    relax: true,
-  }));
+const arrayUnique = (array, prop) => {
+  const values = {};
 
-let headers;
-let counter = 0;
-
-readStream
-  .on('data', (csvRow) => {
-    readStream.pause();
-
-    if (!headers) {
-      headers = csvRow;
-      readStream.resume();
-    } else {
-      const gene = { name: csvRow[0] };
-
-      if (gene.name) {
-        knex.insert(gene).returning('id').into('genes')
-          .then(ids => ids[0])
-          .catch((err) => {
-            // if gene already exists in db
-            if (err.code === '23505') {
-              return knex('genes').where(gene)
-                .then(genes => genes[0].id);
-            }
-
-            throw err;
-          })
-          .then((geneId) => {
-            const variant = {
-              geneId,
-              nucleotideChanges: csvRow[1].split(',')
-                .concat(csvRow[3].split(','))
-                .filter(string => string),
-              proteinChange: csvRow[2],
-              alias: csvRow[4],
-              region: csvRow[6],
-              reportedClassification: csvRow[7],
-              source: csvRow[9],
-              lastEvaluated: csvRow[11] || null,
-              lastUpdated: csvRow[10] || null,
-              url: csvRow[12],
-            };
-
-            return knex('variants').insert(variant);
-          })
-          .then(() => {
-            if (counter++ % 25 === 0) {
-              console.log(`inserted ${counter} records`);
-            }
-            readStream.resume();
-          });
-      } else {
-        readStream.resume();
-      }
-    }
-  })
-  .on('end', () => {
-    console.log('done!');
+  array.forEach((el) => {
+    values[el[prop]] = values[el[prop]] || el;
   });
 
+  return Object.values(values);
+};
+
+knex('variants').del()
+  .then(() => knex('genes').del())
+  .then(() => {
+    const readStream = fs.createReadStream(process.env.DB_FILE_PATH)
+      .pipe(parse({
+        delimiter: '\t',
+        relax_column_count: true,
+        relax: true,
+        from: 2,
+      }));
+
+    let rows = [];
+    const geneIds = {};
+    let counter = 0;
+
+    const insertIntoDB = () => {
+      const newGenes = arrayUnique(rows.map(row => row.gene)
+        .filter(gene => !geneIds[gene.name]), 'name');
+      return knex.batchInsert('genes', newGenes, 100)
+        .returning('id')
+        .then((ids) => {
+          newGenes.forEach((gene, i) => {
+            geneIds[gene.name] = ids[i];
+          });
+
+          for (let i = 0; i < rows.length; i++) {
+            rows[i].variant.geneId = geneIds[rows[i].gene.name];
+          }
+
+          return knex.batchInsert('variants', rows.map(row => row.variant));
+        });
+    };
+
+    readStream
+      .on('data', (csvRow) => {
+        const gene = { name: csvRow[0] };
+
+        if (gene.name) {
+          const variant = {
+            nucleotideChanges: csvRow[1].split(',')
+              .concat(csvRow[3].split(','))
+              .filter(string => string),
+            proteinChange: csvRow[2],
+            alias: csvRow[4],
+            region: csvRow[6],
+            reportedClassification: csvRow[7],
+            source: csvRow[9],
+            lastEvaluated: csvRow[11] || null,
+            lastUpdated: csvRow[10] || null,
+            url: csvRow[12],
+          };
+
+          const row = { gene, variant };
+          rows.push(row);
+
+          if (rows.length % 1000 === 0) {
+            readStream.pause();
+            insertIntoDB()
+              .then(() => {
+                rows = [];
+                readStream.resume();
+                console.log(counter += 1000);
+              });
+          }
+        }
+      })
+      .on('end', () => {
+        insertIntoDB();
+        console.log('done!');
+      });
+  });
